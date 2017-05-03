1b57060b4e4f656f19d8203e64a39fa043741b6e
Josh Horecny <jphorec@gmail.com>
2017-05-03 08:11:24 -0500

    remove security group, use aws default

--- aws/main.tf
+++ aws/main.tf
@@ -2,22 +2,6 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "allow_all"
-  }
-}
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -35,9 +19,6 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  security_groups = [
-    "${aws_security_group.allow_all.id}",
-  ]
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name = "brownbag"
