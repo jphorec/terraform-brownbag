b00682c023b9f0be6f325d1171ce93a14c74433a
Josh Horecny <jphorec@gmail.com>
2017-05-02 22:42:34 -0500

    add security group to allow all access fro minternet

--- aws/main.tf
+++ aws/main.tf
@@ -2,31 +2,46 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "allow_all"
+  }
+}
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-filter {
+  filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  
-}
+  }
 
-filter {
+  filter {
     name   = "virtualization-type"
     values = ["hvm"]
-  
-}
+  }
 
   owners = ["099720109477"] # Canonical
-
 }
 
 resource "aws_instance" "web" {
+  security_groups = [
+    "${aws_security_group.allow_all.id}",
+  ]
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name = "brownbag"
-tags {
-    Name = "Brownbag"
-  
-}
+  tags {
+    Name = "brownbag"
+  }
 }
