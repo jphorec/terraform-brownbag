1fee3d7815e228a9ac2632eeec25559d555a455d
jphorec <jphorec@gmail.com>
2017-06-22 11:17:34 -0500

    remove ami definition

--- aws/main.tf
+++ aws/main.tf
@@ -2,22 +2,6 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-/*data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-*/
 resource "aws_instance" "brownbag" {
   ami           = "ami-46134b51"
   instance_type = "t2.micro"
