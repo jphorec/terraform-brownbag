bcee5e78c2176c83e6e431c48d9134b1abb493ac
jphorec <jphorec@gmail.com>
2017-06-21 22:32:49 -0500

    working state

--- aws/main.tf
+++ aws/main.tf
@@ -21,12 +21,12 @@ provider "aws" {
 resource "aws_instance" "brownbag" {
   ami           = "ami-46134b51"
   instance_type = "t2.micro"
-  key_name = "brownbag"
+  key_name = "JoshKey"
   tags {
     Name = "brownbag"
   }
 }
 
 output "connection info" {
-  value = "ssh -i ~/.ssh/brownbag.pem ec2-user@${aws_instance.brownbag.public_dns}"
+  value = "ssh -i ~/.ssh/${aws_instance.brownbag.key_name}.pem ec2-user@${aws_instance.brownbag.public_dns}"
 }
